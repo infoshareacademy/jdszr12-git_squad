91e7954f02e07922984daec3dc875280447ce32a
Anna Krzeminska <anna.paulina.krzeminska@gmail.com>
2023-03-03 10:53:06 +0100

    Added group by year and quarter

--- analiza wartosci do odrzutu.sql
+++ analiza wartosci do odrzutu.sql
@@ -139,7 +139,7 @@ from
 
 
 ====================================================================================
------GRUPOWANIE DLA CITY1 - MIN, MAX, SUM, SREDNIA--------------
+-----GRUPOWANIE DLA CITY1 I CITY2- MIN, MAX, SUM, SREDNIA--------------
 ====================================================================================
 ----->>>> dla CITY1
 
@@ -200,7 +200,7 @@ group by 1 order by 1;
 
 
 ====================================================================================
------WYSZUKANIE PUSTYCH PRZELOTOW (calkowicie lub tylko passengers/freight--------------
+-----WYSZUKANIE PUSTYCH PRZELOTOW (calkowicie lub tylko passengers/freight----------
 ====================================================================================
 
 ---->>>> Całkowicie puste przeloty = 185 przelotow
@@ -218,8 +218,6 @@ select * from citypairwise c
 where ("PASSENGERS FROM CITY1 TO CITY2" > 0 OR "PASSENGERS FROM CITY2 TO CITY1" >0) and "FREIGHT FROM CITY1 TO CITY2" = 0 and "FREIGHT FROM CITY2 TO CITY1" = 0;
 
 
-
-
 --->>>> przeloty tylko z ładunkiem = 47 przelotow (w obie strony jest ładunek), 103 przeloty (przynajmniej w jedna strone jest ładunek)
 
 select * from citypairwise c 
@@ -229,7 +227,66 @@ select * from citypairwise c
 where "PASSENGERS FROM CITY1 TO CITY2" = 0 and "PASSENGERS FROM CITY2 TO CITY1" = 0 and ("FREIGHT FROM CITY1 TO CITY2" > 0 OR "FREIGHT FROM CITY2 TO CITY1" > 0);
 
 
+===================================================================================
+----- GRUPOWANIE PO ROKU I KWARTALE -----------------------------------------------
+===================================================================================
 
+---->>> Grupowanie po roku 
+
+select "YEAR",  
+	sum("PASSENGERS FROM CITY1 TO CITY2") as P_TO_I, 
+	sum("PASSENGERS FROM CITY2 TO CITY1") as P_FROM_I, 
+	round(sum("FREIGHT FROM CITY1 TO CITY2")::numeric, 2) as F_TO_I, 
+	round(sum("FREIGHT FROM CITY2 TO CITY1")::numeric,2) as F_FROM_I 
+from citypairwise c 
+group by 1 order by 1;
+
+YEAR|p_to_i  |p_from_i|f_to_i   |f_from_i |
+----+--------+--------+---------+---------+
+2015|23781640|24858956|560738.00|887775.00|
+2016|26807724|27771398|587550.00|897475.00|
+2017| 6521248| 6775745|137174.00|231437.00|
+
+
+select "YEAR", city1, city2,  
+	sum("PASSENGERS FROM CITY1 TO CITY2") as P_TO_I, 
+	sum("PASSENGERS FROM CITY2 TO CITY1") as P_FROM_I, 
+	round(sum("FREIGHT FROM CITY1 TO CITY2")::numeric, 2) as F_TO_I, 
+	round(sum("FREIGHT FROM CITY2 TO CITY1")::numeric,2) as F_FROM_I 
+from citypairwise c 
+group by 1,2,3 order by 1;
+
+---->>>Grupowanie po kwartale
+
+select "YEAR", quarter,  
+	sum("PASSENGERS FROM CITY1 TO CITY2") as P_TO_I, 
+	sum("PASSENGERS FROM CITY2 TO CITY1") as P_FROM_I, 
+	round(sum("FREIGHT FROM CITY1 TO CITY2")::numeric, 2) as F_TO_I, 
+	round(sum("FREIGHT FROM CITY2 TO CITY1")::numeric,2) as F_FROM_I 
+from citypairwise c 
+group by 1,2 order by 1,2;
+
+YEAR|quarter|p_to_i |p_from_i|f_to_i   |f_from_i |
+----+-------+-------+--------+---------+---------+
+2015|Q1     |5712230| 6376365|126382.00|213763.00|
+2015|Q2     |5876964| 6276379|138741.00|214714.00|
+2015|Q3     |5711102| 6064483|150642.00|205418.00|
+2015|Q4     |6481344| 6141729|144974.00|253880.00|
+2016|Q1     |6343530| 6871288|134226.00|215836.00|
+2016|Q2     |6778304| 7424671|154550.00|237738.00|
+2016|Q3     |7170031| 6672921|153139.00|224225.00|
+2016|Q4     |6515859| 6802518|145634.00|219676.00|
+2017|Q1     |6521248| 6775745|137174.00|231437.00|
+
+
+
+select "YEAR", quarter, city1, city2,
+	sum("PASSENGERS FROM CITY1 TO CITY2") as P_TO_I, 
+	sum("PASSENGERS FROM CITY2 TO CITY1") as P_FROM_I, 
+	round(sum("FREIGHT FROM CITY1 TO CITY2")::numeric, 2) as F_TO_I, 
+	round(sum("FREIGHT FROM CITY2 TO CITY1")::numeric, 2) as F_FROM_I 
+from citypairwise c 
+group by 1, 2, 3, 4 order by 1, 2;
 
 
 
